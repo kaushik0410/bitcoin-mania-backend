c8e8ad17102a19c3b4d1412f965df207e2084936
Kaushik Mahadev Bharshing <155575296+kaushik0410@users.noreply.github.com>
2025-05-03 19:25:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,15 +16,18 @@ const allowedOrigins = [
   'https://bitcoin-mania-frontend.onrender.com'
 ];
 
+// app.use(cors({
+//     origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// }));
 app.use(cors({
-    origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: '*',
 }));
 app.use(express.json());
 
